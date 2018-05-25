f75eaa6059879eafca04115b9c77094b31888f19
Aron Roberts <robotros1@msn.com>
2018-05-25 09:49:22 -0700

    feat: created array to store deck

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 /*
  * Create a list that holds all of your cards
  */
-
-
+const deck = ["fa fa-diamond",
+	"fa fa-diamond",
+	"fa fa-paper-plane-o",
+	"fa fa-paper-plane-o",
+	"fa fa-anchor",
+	"fa fa-anchor",
+	"fa fa-bolt",
+	"fa fa-bolt",
+	"fa fa-cube",
+	"fa fa-cube",
+	"fa fa-leaf",
+	"fa fa-leaf"
+	"fa fa-bicycle",
+	"fa fa-bicycle",
+	"fa fa-bomb",
+	"fa fa-bomb"]
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -24,6 +38,7 @@ function shuffle(array) {
 
     return array;
 }
+
 
 
 /*
