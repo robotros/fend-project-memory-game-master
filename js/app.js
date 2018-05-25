5b0f23e33ec9fb781fb5e070be29a770f5901e8f
Aron Roberts <robotros1@msn.com>
2018-05-25 15:02:13 -0700

    feat: major update
    added most game functionality.

--- js/app.js
+++ js/app.js
@@ -25,7 +25,18 @@ const deck = $(".deck");
 const restart = $(".restart");
 
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+//variables
+let open = [];
+let turns = 0;
+let score =0;
+const threeStar = 20;
+const twoStar =25;
+
+/**
+* @description Shuffle function from http://stackoverflow.com/a/2450976
+* @param array - an array that should be shuffled
+* @return array - a new array with randomized index
+*/
 function shuffle(array) {
     let currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -40,19 +51,38 @@ function shuffle(array) {
     return array;
 };
 
+function resetTurns(){
+	turns=0;
+	moves.html(turns);
+}
 
+function resetStars(){
+
+}
+
+/**
+* @description Starts new game
+*/
 function startGame() {
 	// shuffle the list of cards using the "shuffle" method
+	resetTurns();
+	resetStars();
+	score =0;
 	let newDeck = shuffle(starterDeck);
 	let c = document.createDocumentFragment();
-	//loop through each card and create its HTML
+
+	// loop through each card and create its HTML
 	newDeck.forEach(function(cards) {
+
+		// create li element
     	const card = document.createElement("li");
     	card.className = "card";
 
+    	// create i element
     	const symbol = document.createElement("i");
     	symbol.className = cards;
 
+    	// append to fragments
     	card.appendChild(symbol);
    	 	c.appendChild(card);
 	});
@@ -63,23 +93,62 @@ function startGame() {
 };
 
 /**
-* @description sets selected color
+* @description listener for restart button
 */
 restart.click(function(event) {
 	startGame();
 });
 
+function endTurn(){
+	turns++;
+	moves.html(turns);
+	if (score > 7){
+		startGame();
+	}
+}
 
+function showCard(card){
+	card.addClass("open show");
+};
+
+function hideCard(card){
+	card.removeClass("open show");
+};
+
+function matchCard(pair){
+	// add match class
+	pair[0].addClass("match");
+	pair[1].addClass("match");
+	score++;
+};
+
+
+function openCard(card){
+	open.push(card);
+
+	if (open.length > 1){
+		if (open[0].find("i").attr("class") === open[1].find("i").attr("class")) {
+			matchCard(open);
+		}
+			hideCard(open.pop());
+			hideCard(open.pop());
+			endTurn();
+	}
+}
+
+/**
+* @description listener for a card.
+*/
 deck.on("click", ".card", function(event) {
-      $(this).addClass("open");
+	if (!$(this).hasClass("open") && !$(this).hasClass("match")){
+		showCard($(this));
+      	openCard($(this));
+	} else {
+		hideCard(open.pop());
+		endTurn();
+	}
   });
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+/* TODO:
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
